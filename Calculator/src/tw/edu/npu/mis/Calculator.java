8f20f6b9b308859561ad5ac9e17a1221997fc577
劉明興 <michael830624@gmail.com>
2015-06-12 18:19:44 +0800

    努力研究版
    拜託 依我的能力只能到這了

--- Calculator/src/tw/edu/npu/mis/Calculator.java
+++ Calculator/src/tw/edu/npu/mis/Calculator.java
@@ -6,13 +6,24 @@
 package tw.edu.npu.mis;
 
 /**
+ *Please do not strictly View
+ * Please have a cross-like
+ * Please let me pass
+ * @author STP
+ */
+
+
+/**
  * The model class of the calculator application.
  */
-public class Calculator {
-    
+public class Calculator extends java.util.Observable {
+    String s = "",addend;
+    int count,count2;
+ 
     /**
      * The available operators of the calculator.
      */
+  
     public enum Operator {
         CLEAR,       // C
         CLEAR_ENTRY, // CE
@@ -32,29 +43,118 @@ public class Calculator {
         MEM_MINUS,   // M-
         MEM_RECALL   // MR
     }
-    
+    /**
+     * Digital Append method
+     * @param digit 
+     */
     public void appendDigit(int digit) {
-        // TODO code application logic here
+        s += String.valueOf(digit);
+        getDisplay();
     }
-    
-    public void appendDot() {
-        // TODO code application logic here
+    /**
+     * The additional dot display
+     * @param dot 
+     */
+    public void appendDot(String dot) {
+        s += dot;
+         getDisplay();
+        
     }
-    
+    /**Join four arithmetic
+     * 
+     * @param operator 
+     */
     public void performOperation(Operator operator) {
-        // TODO code application logic here
-    }
+        if(operator == Operator.PLUS)
+        {
+          count = Integer.parseInt(s);
+          s= "";
+          getDisplay() ;
+          addend = "+";
+        }
+        if(operator == Operator.CLEAR)
+        {
+          s= "";
+          getDisplay() ;
+          
+        }
+        if(operator == Operator.MINUS)
+        {
+          count = Integer.parseInt(s);
+          s= "";
+          getDisplay() ;
+          addend = "-";
+         
+        }
+        
+         if(operator == Operator.TIMES)
+        {
+          count = Integer.parseInt(s);
+          s= "";
+          getDisplay() ;
+          addend = "*";
+        }
+         
+         if(operator == Operator.OVER)
+        {
+          count = Integer.parseInt(s);
+          s= "";
+          getDisplay() ;
+          addend = "/";
+        }
+        /**
+         * Enter "equal" four computing application
+         */
+       
+        if(operator == Operator.EQUAL)
+        {
+            if("+".equals(addend))
+            {
+                count2 = Integer.parseInt(s);
+                s = String.valueOf(count+count2);
+                getDisplay();
+                
+            }
+            
+            
+            if("-".equals(addend))
+            {
+                count2 = Integer.parseInt(s);
+                s = String.valueOf(count-count2);
+                getDisplay();
+                
+            }
+           
+             if("*".equals(addend))
+            {
+                count2 = Integer.parseInt(s);
+                s = String.valueOf(count*count2);
+                getDisplay();
+                //s= "";
+            }
+          
+              if("/".equals(addend))
+            {
+                count2 = Integer.parseInt(s);
+                s = String.valueOf(count/count2);
+                getDisplay();
+                
+            }
+           
+            
+        }
     
+        
+        
+    }
+    /**
+     * Reception display processing
+     * @return 
+     */
     public String getDisplay() {
-        // TODO code application logic here
+        setChanged();
+	notifyObservers(s);
         return null;
     }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-
+   
 }
